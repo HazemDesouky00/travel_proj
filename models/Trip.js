71cd8baef28b9e6783ac6f81e05eba2f706d9c25
HazemDesouky00 <HD2400936@tkh.edu.eg>
2025-10-21 11:17:58 +0300

    travel project

---
+++ models/Trip.js
@@ -0,0 +1,31 @@
+const trips = [
+    {
+        id:1,
+        destinationName:'Paris',
+        location:'France',
+        continent: 'Europe',
+        language:'French',
+        description: 'Romantic city known for art, cuisine, and the Eiffel Tower.',
+        flightCost: 400,
+        accommodationCost: 150,
+        mealCost: 80,
+        visaCost: 50,
+        transportationCost: 30,
+        currencyCode: 'EUR',
+
+    },
+    {
+        id: 2,
+        destinationName: 'Tokyo',
+        location: 'Japan',
+        continent: 'Asia',
+        language: 'Japanese',
+        description: 'A vibrant metropolis blending ancient culture and modern innovation.',
+        flightCost: 800,
+        accommodationCost: 200,
+        mealCost: 90,
+        visaCost: 0,
+        transportationCost: 40,
+        currencyCode: 'JPY',
+    }
+]
